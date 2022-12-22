c947da381cea5ca544f37623fc39df2c75e18fb5
talimbayev <alimbayev.talgat@gmail.com>
2022-12-22 15:12:49 +0600

    starting with front

---
+++ talgat-demo-store-front/src/main/java/talgat/demo/store/front/TalgatDemoStoreFrontApplication.java
@@ -0,0 +1,13 @@
+package talgat.demo.store.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalgatDemoStoreFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalgatDemoStoreFrontApplication.class, args);
+	}
+
+}
---
+++ talgat-demo-store-front/src/test/java/talgat/demo/store/front/TalgatDemoStoreFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package talgat.demo.store.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalgatDemoStoreFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
